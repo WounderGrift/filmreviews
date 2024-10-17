a622e1bf1bab5e48962e3c884db3ba17d4d9ed11
Dmitrii <woundergrift@gmail.com>
2024-10-17 12:59:28 +0400

    greensteam v3

--- app/Http/Helpers/TextHelper.php
+++ app/Http/Helpers/TextHelper.php
@@ -6,7 +6,7 @@ class TextHelper
 {
     public static function fiveSentences(string $text, int $length): string
     {
-        $sentences = preg_split('/(?<=[.!?])\s+/', $text, -1, PREG_SPLIT_NO_EMPTY);
+        $sentences = preg_split('/(?<=[.!?])\s+/u', $text, -1, PREG_SPLIT_NO_EMPTY);
         $firstFiveSentences = array_slice($sentences, 0, $length);
         return implode(' ', $firstFiveSentences);
     }
